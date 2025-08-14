652a36425b4c06e76c1141f31eebd0b212c0c87d
maxinth <tunde.j.adeleye@gmail.com>
2025-08-14 19:54:36 +0100

    chart - init.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import CalendarSidebar from "./components/Calendar";
+import GroupedBarChart from "./components/LineChart";
 
 function App() {
   return (
     <main className="!font-ecb w-full max-w-[1440px] mx-auto">
       <section className=" w-full">hello</section>
       <CalendarSidebar />
+      <div className="max-w-[300px]">
+        <GroupedBarChart />
+      </div>
     </main>
   );
 }
---
+++ src/components/LineChart.tsx
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function GroupedBarChart() {
+  const labels = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Series 1",
+        data: [35, 8, 14, 12, 8, 32, 22, 20, 31],
+        backgroundColor: "#4545FE",
+        barPercentage: 0.6,
+        categoryPercentage: 0.8,
+      },
+      {
+        label: "Series 2",
+        data: [28, 27, 7, 25, 4, 45, 31, 8, 29],
+        backgroundColor: "#12B76A",
+        barPercentage: 0.6,
+        categoryPercentage: 0.8,
+      },
+      {
+        label: "Series 3",
+        data: [10, 9, 4, 0, 8, 7, 15, 15, 8],
+        backgroundColor: "#F04438",
+        barPercentage: 0.6,
+        categoryPercentage: 0.8,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        mode: "index" as const,
+        intersect: false,
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: false, // ⬅ removes horizontal grid lines
+        },
+        ticks: {
+          callback(this: any, value: string | number) {
+            return `${value}m`;
+          },
+        },
+      },
+      x: {
+        grid: {
+          display: false, // ⬅ removes vertical grid lines
+        },
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
+}
